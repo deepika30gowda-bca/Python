69f37801528f9aa26cca9999da60c321f3f5cc23
deepika30gowda-bca <deepika30gowda@gmail.com>
2026-07-18 14:45:37 +0530

    Create Prime.py

---
+++ Prime.py
@@ -0,0 +1,10 @@
+print("Prime numbers between 1 and 50 are:")
+
+for num in range(1, 51):
+    if num > 1:
+        for i in range(2, int(num ** 0.5) + 1):
+            if (num % i) == 0:
+                break
+        else:
+            print(num, end=" ")
+          
